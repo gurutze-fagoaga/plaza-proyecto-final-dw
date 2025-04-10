fc42d7bc3d1104157c1c298a3b872b1b2980311f
gurutze-fagoaga <gurutzefagoaga@alumnos.cei.es>
2025-04-10 12:43:36 +0200

    talleres js

--- js/talleres.js
+++ js/talleres.js
@@ -1,15 +1,43 @@
 const container = document.querySelector("#Main");
 const listaTitulos = container.querySelectorAll("#Nav-item");
 const listaBloques = container.querySelectorAll("#Contenido-bloque");
+const listaPasos = container.querySelectorAll("#Paso");
+
 
 
 listaTitulos.forEach((titulo, i) => {
-    listaTitulos[i].addEventListener("click", () =>{
+    listaTitulos[i].addEventListener("click", () => {
+
+
+        // desactivar el botón que estaba activo
         listaTitulos.forEach((titulo, i) => {
-            listaTitulos[i].classList.remove("u-activo");
+            titulo.classList.remove("u-activo");
             listaBloques[i].classList.remove("u-activo");
         })
+        // activar el botón al que se le ha hecho click
         listaTitulos[i].classList.add("u-activo");
         listaBloques[i].classList.add("u-activo");
+
     })
 })
+
+
+listaPasos.forEach((paso, i) => {
+    paso.addEventListener("click", () => {
+   
+        // desactivar el botón que estaba activo
+        listaTitulos.forEach((titulo, i) => {
+            titulo.classList.remove("u-activo");
+            listaBloques[i].classList.remove("u-activo");
+        })
+        // activar el botón al que se le ha hecho click
+        listaTitulos[i].classList.add("u-activo");
+        listaBloques[i].classList.add("u-activo");
+
+    })
+})
+
+
+
+
+
