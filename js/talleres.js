e958e25844427a4d0179c290bdc29d7f6733627b
gurutze-fagoaga <gurutzefagoaga@alumnos.cei.es>
2025-04-16 12:59:22 +0200

    formulario

--- js/talleres.js
+++ js/talleres.js
@@ -37,24 +37,48 @@ listaPasos.forEach((paso, i) => {
     })
 })
 
-const selectorDeClase = document.querySelector("#SelectClase");
-const listaDeTalleres = document.querySelectorAll("#Clase");
-const listaDeFechas = document.querySelectorAll("#Fecha");
+
+const contenedorReserva = document.querySelector("#Reserva-formClase");
+const selectorFecha = document.querySelector("#Fecha");
+const selectorClase = document.querySelector("#SelectClase");
+
+const listaDeHoras = {
+    ceramica: ["Lunes 5 de mayo, 10:00-14:00", "Jueves 8 de mayo, 18:00-20:00"],
+    joyeria: ["Lunes 5 de mayo, 10:00-14:00", "Jueves 8 de mayo, 18:00-20:00"],
+    bordado: ["Lunes 5 de mayo, 10:00-14:00", "Jueves 8 de mayo, 18:00-20:00"],
+    serigrafia: ["Lunes 5 de mayo, 10:00-14:00", "Jueves 8 de mayo, 18:00-20:00"],
+    escritura: ["Lunes 5 de mayo, 10:00-14:00", "Jueves 8 de mayo, 18:00-20:00"],
+    encuadernacion: ["Lunes 5 de mayo, 10:00-14:00", "Jueves 8 de mayo, 18:00-20:00"]
+}
 
 
-selectorDeClase.addEventListener("click", () => {
+let claseSeleccionada = "none"
 
-    listaDeTalleres.forEach((taller, id) => {
-        listaDeFechas.forEach((fecha, idx) => {
-            listaDeFechas[id].clsssList.remove("u-displayNone");
-        })
-    })
+// en cada click en el selector actualizará el valor de lo seleccionado:
+
+selectorClase.addEventListener("click", () => {
+
+    let claseSeleccionada = selectorClase.value
+    console.log(claseSeleccionada);
+
+    
+    selectorFecha.classList.remove("u-displayNone");
 
 })
 
+// funcion para añadir las opciones segun el valor del selector de clase
+
+console.log(selectorClase[1].value);
+
+console.log(listaDeHoras.ceramica[1]);
 
 
+for(i=0 ; i< listaDeHoras.ceramica.length; i++){
 
+    selectorFecha.innerHTML+= `
+          <option id="Hora" value="${listaDeHoras.ceramica[i]}">${listaDeHoras.ceramica[i]}</option>
+    `
+}
 
 
 
