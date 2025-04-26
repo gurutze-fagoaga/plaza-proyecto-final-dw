e4cad64e794e6c7161b76bf32297d3e4232065cd
gurutze-fagoaga <gurutzefagoaga@alumnos.cei.es>
2025-04-26 13:20:17 +0200

    boton subir

--- js/js.js
+++ js/js.js
@@ -11,12 +11,36 @@
 
 // ------------------------- CONSTANTES Y VARIABLES ------------------------------- //
 
+// Selección del botón de subir
+const btnSubir = document.querySelector("#Main-subir");
 // Selección de los iconos del menu hamburguesa (boton para abrir y cerrar menu)
 const listaIconosMenu = document.querySelectorAll(".Header-icon");
 // Selección del menu
 const menu = document.querySelector(".Menu");
 // Selección de los items de la lista del menu
 const listaBotonesMenu = document.querySelectorAll(".Menu-item");
+
+
+// Función para mostrar el botón de subir arriba
+// añado event listener de "wheel" (rueda de scroll del ratón) al documento
+document.addEventListener('wheel', (e) => {
+    // si el scroll no está al inicio (!=0) 
+    if (window.scrollY != 0){
+        // se muestra el botón
+        btnSubir.classList.remove("u-displayNone") 
+    // si está al inicio
+    } else {
+        // añade el botón
+        btnSubir.classList.add("u-displayNone") 
+    }
+    
+});
+// Función para hacer scroll arriba con el botón de subir
+btnSubir.addEventListener("click", () => {
+    window.scrollTo(0,0)
+    btnSubir.classList.add("u-displayNone")  
+})
+
 
 // Función para desplegar y plegar el menu hamburguesa
 // recorre los iconos
@@ -33,6 +57,9 @@ listaIconosMenu.forEach(icono => {
         })
     })
 });
+
+
+
 
 
 
